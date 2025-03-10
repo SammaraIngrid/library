426c90ef887c36f6a47b907f8f4fa19202e13957
Sammara Ingrid <sammaracavalcante2@gmail.com>
2025-03-10 18:45:45 -0300

    correcao da migracao Bookcase

--- db/migrate/20250310151545_create_bookcases.rb
+++ db/migrate/20250310151545_create_bookcases.rb
@@ -2,7 +2,7 @@ class CreateBookcases < ActiveRecord::Migration[7.2]
   def change
     create_table :bookcases do |t|
       t.integer :limit
-      t.string "name"
+      t.string :name
 
       t.timestamps
     end
