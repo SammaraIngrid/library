cb3047e9d3ffdcf618f1cab8969796138e11e5a1
sammaraingrid <sammaracavalcante2@gmail.com>
2025-04-22 14:41:24 -0300

    Adiciona as rotas para autenticação com Devise e Devise Token Auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for "User", at: "auth"
+  as :user do
+    # Define routes for User within this block.
+  end
   resources :books
   resources :authors
   resources :bookcases
